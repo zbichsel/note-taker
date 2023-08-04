d46e6dcef2574a46994facd788e3e82d21bce3d1
Zackrey Bichsel <biksel09@hotmail.com>
2023-08-03 21:00:43 -0500

    added starter code

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middlware/clog');
+const api = require('./public/assets/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at https://localhost:${PORT} 🚀`)
+);
